35c4197f22c9fa55aa52a8d58d0360a81a7b749c
Kavindu Lakshan Munasinghe <104860051+LakshanKavindu@users.noreply.github.com>
2024-04-05 11:26:49 +0530

    installed flowbite components library

--- pramuka/src/App.jsx
+++ pramuka/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
-
+import { Button } from "flowbite-react";
+import { Badge } from "flowbite-react";
 function App() {
 
   return (
@@ -8,6 +9,12 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <div>
+        <Button>Click me</Button>
+        <Badge size="sm" href="#">
+          Default
+        </Badge>
+      </div>
     </>
   )
 }
