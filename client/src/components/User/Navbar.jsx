9adb980ebadb4d67dc59dee8c3fa3b84d6b79f95
Kavindu Lakshan Munasinghe <104860051+LakshanKavindu@users.noreply.github.com>
2024-04-17 10:56:46 +0530

    Add Button component and update Add to Cart button in ProductCard.jsx

--- client/src/components/User/Navbar.jsx
+++ client/src/components/User/Navbar.jsx
@@ -12,13 +12,21 @@ const Nav = ({ isActive }) => {
                 {/* <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" /> */}
                 <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Pramuka Products</span>
             </Navbar.Brand>
-            <div className="flex gap-4 md:order-2">
-                <FiShoppingCart className=" cursor-pointer" />
-                <FaRegUserCircle className=" cursor-pointer" />
+            <div className="flex gap-4 md:order-2 items-center pt-2 ">
+                <div className="    flex justify-center items-center">
+                    <div className="relative mr-2">
+                        <div className="absolute left-4 bottom-4">
+                            <p className="flex h-1 w-1 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white">3</p>
+                        </div>
+                        <FiShoppingCart className="cursor-pointer text-2xl" />
+
+                    </div>
+                </div>
+                <FaRegUserCircle className=" cursor-pointer text-2xl" />
                 <Navbar.Toggle />
             </div>
             <Navbar.Collapse>
-                <Navbar.Link href="#" className={`hover:text-primary ${isActive === "home" ? "text-primary" : ""}`}>Home</Navbar.Link>
+                <Navbar.Link href="/user" className={`hover:text-primary ${isActive === "home" ? "text-primary" : ""}`}>Home</Navbar.Link>
                 <Navbar.Link href="#" className="hover:text-primary">About</Navbar.Link>
                 <Navbar.Link href="#" className="hover:text-primary">Pricing</Navbar.Link>
                 <Navbar.Link href="#" className="hover:text-primary">Contact</Navbar.Link>
--- client/src/components/User/ProductCard.jsx
+++ client/src/components/User/ProductCard.jsx
@@ -1,5 +1,5 @@
 
-import { Card } from "flowbite-react"
+import { Button, Card } from "flowbite-react"
 // import Image from "next/image"
 const ProductCard = () => {
   return (
@@ -17,12 +17,13 @@ const ProductCard = () => {
 
       <div className="flex items-center justify-between">
         <span className="text-2xl font-bold text-gray-900 dark:text-white">$599</span>
-        <a
+        {/* <a
           href="#"
           className="rounded-lg bg-primary hover:bg-orange-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
         >
           Add to cart
-        </a>
+        </a> */}
+        <Button size="sm" gradientDuoTone="primary">Add to Cart</Button>
       </div>
     </Card>
 
