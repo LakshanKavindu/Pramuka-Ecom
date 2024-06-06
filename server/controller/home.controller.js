e829e13fd20e205a499a37b76b4ebf863bcd649a
Nipun Madhushan <108778265+Nipun2001M@users.noreply.github.com>
2024-06-06 21:54:53 +0530

    all_available_products controller created in home.controller file

--- server/controller/home.controller.js
+++ server/controller/home.controller.js
@@ -1,5 +1,15 @@
 
 
 const all_available_products=async()=>{
-    
+    try{
+        const products=await all_products();
+        res.status(200).send({products})
+        
+
+    }catch(e){
+        res.status(400).send({"error":e})
+
+
+    }
+        
 }
