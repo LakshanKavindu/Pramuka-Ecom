87964930ce145c887f2f5a2978c6d27f1c62077e
Aruna Priyadarshana Bandara <108753678+arunapriyadarshana@users.noreply.github.com>
2024-06-09 15:18:47 +0530

    update code

--- server/controller/admin.controller.js
+++ server/controller/admin.controller.js
@@ -1,27 +1,22 @@
-// import { createProduct } from "../service/admin.service.js";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import { createProduct } from "../service/admin.service.js";
 
 export const addProduct = async (req, res) => {
-    const values = req.body;
-    const { name, description, brand, category, image, stock, price } = values;
-    console.log(values, "product")
-    try {
-        await prisma.product.create({
-            data: {
-                productName: name,
-                productDescription: description,
-                productBrand: brand,
-                productCategory: category,
-                productImage: image,
-                productStock: parseInt(stock),
-                productPrice: parseInt(price)
-            }
-        })
-
-        res.status(201).send({ message: "Product added successfully" });
-    } catch (e) {
-        res.status(500).send({ error: e });
-    }
-
-}
+  const values = req.body;
+  const { name, description, brand, category, image, stock, price } = values;
+  console.log(values, "product");
+  try {
+    await createProduct({
+      name: "abc",
+      description: "def",
+      brand: "ghi",
+      category: "jkl",
+      image: "mno",
+      stock: 2,
+      price: 123,
+    });
+    res.status(201).send({ message: "Product added successfully" });
+  } catch (e) {
+    console.log(e);
+    res.status(500).send({ error: e });
+  }
+};
--- server/service/admin.service.js
+++ server/service/admin.service.js
@@ -1,6 +1,25 @@
-// import { PrismaClient } from "@prisma/client";
-// const prisma = new PrismaClient();
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-// export const createProduct = async (product) => {
-  
-// };
+export const createProduct = async ({
+  name,
+  description,
+  brand,
+  category,
+  image,
+  stock,
+  price,
+}) => {
+  return await prisma.product.create({
+    data: {
+      productName: name,
+      productDescription: description,
+      productBrand: brand,
+      productCategory: category,
+      productImage: image,
+      productPrice: price,
+      productStock: stock,
+      productSold: 0,
+    },
+  });
+};
