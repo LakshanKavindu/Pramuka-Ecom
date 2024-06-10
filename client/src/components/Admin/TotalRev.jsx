595d82c6f62a367babd8d87f4d3e1a4e12056b3d
Nipun Madhushan <108778265+Nipun2001M@users.noreply.github.com>
2024-06-10 11:12:48 +0530

    total rev card created and addded to admin dashboard

---
+++ client/src/components/Admin/TotalRev.jsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Card } from "flowbite-react";
+
+const TotalRev=()=>{
+    const today = new Date();
+    
+    return(
+        <Card href="#" className="max-w-sm">
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+       Your Total Revanue 
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+      
+      Your total revenue until {today.toLocaleDateString()} is Rs 230,443.
+      </p>
+    </Card>
+    )
+}
+
+export default TotalRev;
--- client/src/pages/Admin/AdminDashboard.jsx
+++ client/src/pages/Admin/AdminDashboard.jsx
@@ -1,11 +1,17 @@
 import { SideMenu } from "../../components/Admin/SideMenu";
+import TotalRev from "../../components/Admin/TotalRev";
 
 const AdminDashboard = () => {
   return (
     <div className=" w-full flex flex-row">
       <SideMenu />
 
-      <div className="w-full p-6 h-screen">{/* content goes here  */}</div>
+      <div className="w-full p-6 h-screen">
+        <div className="flex-row">
+          <TotalRev/>
+
+        </div>
+      </div>
     </div>
   );
 };
