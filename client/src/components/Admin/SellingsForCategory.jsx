75e64fb04d25256005895815f6957fdc092d7974
Nipun Madhushan <108778265+Nipun2001M@users.noreply.github.com>
2024-06-10 12:58:05 +0530

    sellingsforcategory component connected with backend

--- client/src/components/Admin/SellingsForCategory.jsx
+++ client/src/components/Admin/SellingsForCategory.jsx
@@ -1,8 +1,24 @@
 "use client";
 
+import axios from "axios";
 import { Card } from "flowbite-react";
+import { useEffect, useState } from "react";
 
 const SellingsForCategory = () => {
+    const [catsellings,setCatsellings]=useState([])
+    const getsellings=()=>{
+        axios.get('http://localhost:8080/api/admin/sellings')
+        .then((res)=>{
+            setCatsellings(res.data.sellings)
+           
+        }).catch((error)=>{
+            console.log(error)
+        })
+    }
+    useEffect(()=>{
+        getsellings();
+
+    },[])
   return (
     <Card className="max-w-sm mt-10">
       <h5 className="mb-3 text-base font-semibold text-gray-900 lg:text-xl dark:text-white">Sellings</h5>
@@ -10,40 +26,30 @@ const SellingsForCategory = () => {
         Sellings of Pramuka stores for each product Category
       </p>
       <ul className="my-4 space-y-3">
-        <li>
-          <a
-            href="#"
-            className="group flex items-center justify-between rounded-lg bg-gray-50 p-3 text-base font-bold text-gray-900 hover:bg-gray-100 hover:shadow dark:bg-gray-600 dark:text-white dark:hover:bg-gray-500"
-          >
-            <div className="flex items-center">
-              <img src="https://img.icons8.com/?size=100&id=gD9WHI7lKKYP&format=png&color=000000" className="h-6 w-6" alt="icon" />
-              <span className="ml-3 whitespace-nowrap">Biscuit</span>
-            </div>
-            <span className="flex items-center">
-              <span className="ml-3 flex-1">12</span>
-              <span className="ml-3 inline-flex items-center justify-center rounded bg-gray-200 px-2 py-0.5 text-xs font-medium text-gray-500 dark:bg-gray-700 dark:text-gray-400">
-                Popular
+       {catsellings.map((item,index)=>{
+        return(
+            <div key={index}>
+                <li>
+            <a
+              href="#"
+              className="group flex items-center justify-between rounded-lg bg-gray-50 p-3 text-base font-bold text-gray-900 hover:bg-gray-100 hover:shadow dark:bg-gray-600 dark:text-white dark:hover:bg-gray-500"
+            >
+              <div className="flex items-center">
+                <img src="https://img.icons8.com/?size=100&id=gD9WHI7lKKYP&format=png&color=000000" className="h-6 w-6" alt="icon" />
+                <span className="ml-3 whitespace-nowrap">{item.productCategory}</span>
+              </div>
+              <span className="flex items-center">
+                <span className="ml-3 flex-1">{item._sum.productSold}</span>
+                <span className="ml-3 inline-flex items-center justify-center rounded bg-gray-200 px-2 py-0.5 text-xs font-medium text-gray-500 dark:bg-gray-700 dark:text-gray-400">
+                  Popular
+                </span>
               </span>
-            </span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="group flex items-center justify-between rounded-lg bg-gray-50 p-3 text-base font-bold text-gray-900 hover:bg-gray-100 hover:shadow dark:bg-gray-600 dark:text-white dark:hover:bg-gray-500"
-          >
-            <div className="flex items-center">
-              <img src="https://img.icons8.com/?size=100&id=gD9WHI7lKKYP&format=png&color=000000" className="h-6 w-6" alt="icon" />
-              <span className="ml-3 whitespace-nowrap">Chocolate</span>
+            </a>
+          </li>
             </div>
-            <span className="flex items-center">
-              <span className="ml-3 flex-1">14</span>
-              <span className="ml-3 inline-flex items-center justify-center rounded bg-gray-200 px-2 py-0.5 text-xs font-medium text-gray-500 dark:bg-gray-700 dark:text-gray-400">
-                Popular
-              </span>
-            </span>
-          </a>
-        </li>
+        )
+       })}
+      
       </ul>
     </Card>
   );
