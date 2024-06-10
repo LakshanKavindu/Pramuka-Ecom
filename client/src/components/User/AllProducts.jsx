bd9ec3dda98190b0328c3de9e349b1babb015eaa
Kavindu Lakshan Munasinghe <104860051+LakshanKavindu@users.noreply.github.com>
2024-06-10 09:15:35 +0530

    chore: Add badge to display number of products found in search

--- client/src/components/User/AllProducts.jsx
+++ client/src/components/User/AllProducts.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 import { Button } from "flowbite-react";
 import axios from "axios";
+import { Badge } from "flowbite-react";
+import { IoBagSharp } from "react-icons/io5";
 
 const AllProducts = ({ isSearching, searchresult }) => {
   const [products, SetProducts] = useState([]);
@@ -34,11 +36,18 @@ const AllProducts = ({ isSearching, searchresult }) => {
 
   return (
     <div className="flex flex-col items-center w-full">
-      {isSearching && searchresult.length > 0 && (
-        <div className="text-2xl font-light	mx-8 ">
-          {searchresult.length} products found..
-        </div>
-      )}
+      <div>
+        {isSearching && searchresult.length > 0 && (
+          <Badge
+            icon={IoBagSharp}
+            color="warning"
+            size="md"
+            style={{ padding: "6px 18px" }}
+          >
+            {searchresult.length} products found..
+          </Badge>
+        )}
+      </div>
       <div className="flex flex-wrap justify-center w-full">
         {products.slice(0, visible).map((item) => {
           return <ProductCard key={item.id} item={item} />;
