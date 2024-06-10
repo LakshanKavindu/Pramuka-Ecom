3887293e29e20e9f038d09922454568d9be4c46d
Nipun Madhushan <108778265+Nipun2001M@users.noreply.github.com>
2024-06-10 11:55:25 +0530

    total rev connected with backend

--- client/src/components/Admin/TotalRev.jsx
+++ client/src/components/Admin/TotalRev.jsx
@@ -1,22 +1,45 @@
 "use client";
 
 import { Card } from "flowbite-react";
+import axios from "axios";
+import { useActionData } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-const TotalRev=()=>{
-    // axios.get('')
-    const today = new Date();
-    
-    return(
-        <Card href="#" className="max-w-sm">
+const TotalRev = () => {
+
+  const [totalrevenue, setTotalrevenue] = useState(0);
+  const [today,setToday]=useState( new Date())
+
+  const getTotalRev = () => {
+    console.log("gtr")
+    axios
+      .get("http://localhost:8080/api/admin/totalrevenue")
+      .then((res) => {
+        
+        setTotalrevenue(res.data.TotalRev);
+        console.log(res)
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  
+
+  useEffect(() => {
+    getTotalRev();
+    setToday(new Date())
+  }, []);
+  return (
+    <Card href="#" className="max-w-sm">
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-       Your Total Revanue 
+        Your Total Revanue
       </h5>
       <p className="font-normal text-gray-700 dark:text-gray-400">
-      
-      Your total revenue until {today.toLocaleDateString()} is Rs 230,443.
+        Your total revenue until {today.toLocaleDateString()} is<br></br> <b>Rs {totalrevenue}.00</b>
+       
       </p>
     </Card>
-    )
-}
+  );
+};
 
 export default TotalRev;
