8b69d8721423782a1f48f27a0892a15b2b1e4650
Kavindu Lakshan Munasinghe <104860051+LakshanKavindu@users.noreply.github.com>
2024-06-12 11:03:36 +0530

    style: Update home.css for search bar styling

--- client/src/pages/User/Home.jsx
+++ client/src/pages/User/Home.jsx
@@ -61,89 +61,71 @@ const Home = () => {
       <Nav isActive={"home"} />
       <Slider />
 
-      <div className=" px-20">
-        <form className="max-w-md mt-8 flex min-w-full justify-between flex-wrap   ">
-          <div>
-            <Dropdown
-              outline
-              gradientDuoTone="pinkToOrange"
-              label={<span className="text-black">{filter}</span>}
-              // dismissOnClick={false}
-
-              value={filter}
-            >
-              <Dropdown.Item
-                value="All Products"
-                onClick={() => {
-                  setIsSearching(false);
-                  setFilter("Categories");
-                }}
-              >
-                All Products
-              </Dropdown.Item>
-              <Dropdown.Item
-                value="Chocolate"
-                onClick={() => {
-                  filterhandle("Chocolate");
-                }}
-              >
-                Chocolate
-              </Dropdown.Item>
-              <Dropdown.Item
-                value="Biscuits"
-                onClick={() => {
-                  filterhandle("Biscuit");
-                }}
-              >
-                Biscuits
-              </Dropdown.Item>
-              <Dropdown.Item
-                value="Soap"
-                onClick={() => {
-                  filterhandle("Soap");
-                }}
-              >
-                Soap
-              </Dropdown.Item>
-              <Dropdown.Item
-                value="Toothpaste"
-                onClick={() => {
-                  filterhandle("Toothpaste");
-                }}
-              >
-                Toothpaste
-              </Dropdown.Item>
-            </Dropdown>
-          </div>
+      <div className=" px-20 mb-8">
+        <form className="max-w-md mt-8 flex min-w-full justify-center flex-wrap">
           <label
             htmlFor="default-search"
             className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
           >
             Search
           </label>
-          <div className="relative" style={{ width: "300px" }}>
-            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-              <svg
-                className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
+          <div className="relative w-[700px] min-w-[300px]">
+            <div className="absolute inset-y-0 start-2.5 flex items-center  z-50">
+              <Dropdown
+                outline
+                // gradientDuoTone="pinkToOrange"
+                label={<span className="text-black">{filter}</span>}
+                // dismissOnClick={false}
+                className=" focus:ring-white bg-transparent"
+                value={filter}
               >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
+                <Dropdown.Item
+                  value="All Products"
+                  onClick={() => {
+                    setIsSearching(false);
+                    setFilter("Categories");
+                  }}
+                >
+                  All Products
+                </Dropdown.Item>
+                <Dropdown.Item
+                  value="Chocolate"
+                  onClick={() => {
+                    filterhandle("Chocolate");
+                  }}
+                >
+                  Chocolate
+                </Dropdown.Item>
+                <Dropdown.Item
+                  value="Biscuits"
+                  onClick={() => {
+                    filterhandle("Biscuit");
+                  }}
+                >
+                  Biscuits
+                </Dropdown.Item>
+                <Dropdown.Item
+                  value="Soap"
+                  onClick={() => {
+                    filterhandle("Soap");
+                  }}
+                >
+                  Soap
+                </Dropdown.Item>
+                <Dropdown.Item
+                  value="Toothpaste"
+                  onClick={() => {
+                    filterhandle("Toothpaste");
+                  }}
+                >
+                  Toothpaste
+                </Dropdown.Item>
+              </Dropdown>
             </div>
             <input
               type="search"
               id="default-search"
-              className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search for products"
+              className="block w-full p-4 ps-36 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 z-10"
               required={true}
               onChange={(e) => {
                 setSearchval(e.target.value);
@@ -157,9 +139,26 @@ const Home = () => {
                 handleSearch();
               }}
               disabled={!searchval}
-              className="text-white absolute end-2.5 bottom-2.5 bg-primary hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white w-40 absolute end-2.5 bottom-2.5 bg-primary hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 search_btn"
             >
-              Search
+              <span className="search_bar_text">Your Product</span>
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none search_icon">
+                <svg
+                  className="w-4 h-4 text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
             </button>
           </div>
         </form>
